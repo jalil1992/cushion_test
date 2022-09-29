88da3aff0583dc1385c4603b99edc583282e7f6c
JM <jalilmarkel1992@gmail.com>
2022-09-29 11:36:36 -0400

    models created

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cushion_test
+# Cushion Coding Project: Payment Service
+
+## Requirements
+
+- Build a simple backend using a favorite framework.
+- Database is necessary (can be simple though)
+- 3 mandatory endpoints, 1 bonus endpoint
+- Need to deploy on AWS Beanstalk and provide the IP to the company so that they can register it for callback
+
+## Data provided
+
+- Virtual payment provider at https://bxzggmcc.cushionai.com
+- Payment provider schema at https://bxzggmcc.cushionai.com/schema/
+- Main backend schema at https://app.swaggerhub.com/apis/one10/payment-service_rest_api/v1#/
+
+![API flow](flow.png?raw=true "API flow")
+
+## Implementation
+
+- Django Restframework
+- SQLite
---
+++ payment_service/api/models.py
@@ -0,0 +1,31 @@
+import uuid
+from random import choices
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class AccountStatus(models.TextChoices):
+    current = "current", _("current")
+    delinquent = "delinquent", _("delinquent")
+    closed = "closed", _("closed")
+
+
+class TransactionStatus(models.TextChoices):
+    pending = "pending", _("pending")
+    posted = "posted", _("posted")
+
+
+class Account(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    balance_cents = models.IntegerField(default=0)
+    settled_balance_cents = models.IntegerField(default=0)
+    status = models.CharField(max_length=32, choices=AccountStatus.choices, default=AccountStatus.current)
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="transactions", null=True)
+    amount_cents = models.IntegerField(default=0)
+    merchant_name = models.CharField(max_length=200, default="")
+    status = models.CharField(max_length=32, choices=TransactionStatus.choices, default=TransactionStatus.pending)
+    settled_amount_cents = models.IntegerField(default=0)
